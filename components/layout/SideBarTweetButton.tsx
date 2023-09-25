1e08567ff54857f2e0340c31b15a244a8f60166f
Sovanroth13 <sovanroth016@gmail.com>
2023-09-25 10:00:17 +0700

    update

--- components/layout/SideBarTweetButton.tsx
+++ components/layout/SideBarTweetButton.tsx
@@ -1,17 +1,25 @@
-import useLoginModal from "@/hooks/useLoginModal";
-import { useRouter } from "next/router";
-import React, { useCallback } from "react";
+import { useCallback } from "react";
 import { FaFeather } from "react-icons/fa";
+import { useRouter } from "next/router";
 
-const SideBarTweetButton = () => {
+import useLoginModal from "@/hooks/useLoginModal";
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+const SidebarTweetButton = () => {
   const router = useRouter();
   const loginModal = useLoginModal();
+  const { data: currentUser } = useCurrentUser();
 
-  const onclick = useCallback(() => {
-    loginModal.onOpen();
-  }, [loginModal]);
+  const onClick = useCallback(() => {
+    if (!currentUser) {
+      return loginModal.onOpen();
+    }
+
+    router.push('/');
+  }, [loginModal, router, currentUser]);
+
   return (
-    <div onClick={onclick}>
+    <div onClick={onClick}>
       <div className="
         mt-6
         lg:hidden 
@@ -29,13 +37,31 @@ const SideBarTweetButton = () => {
       ">
         <FaFeather size={24} color="white" />
       </div>
-      <div className=" mt-6 hidden lg:block px-4 py-4 rounded-full bg-sky-500 hover:bg-opacity-90 cursor-pointer transition">
-        <p className=" hidden lg:block text-center font-semibold text-white text-[20px]">
-            Tweet
+      <div className="
+        mt-6
+        hidden 
+        lg:block 
+        px-4
+        py-2
+        rounded-full
+        bg-sky-500
+        hover:bg-opacity-90 
+        cursor-pointer
+      ">
+        <p 
+          className="
+            hidden 
+            lg:block 
+            text-center
+            font-semibold
+            text-white 
+            text-[20px]
+        ">
+          Tweet
         </p>
       </div>
     </div>
   );
 };
 
-export default SideBarTweetButton;
+export default SidebarTweetButton;
